c4ab3c48e5911a873349a3c06bd0a2b1126b9ee2
Markos <markosmk@me.com>
2022-01-26 23:58:52 -0300

    add: model user with hashpass and validation func

---
+++ backend/components/user/model.user.js
@@ -0,0 +1,73 @@
+const bcrypt = require('bcrypt');
+const { DataTypes } = require('sequelize');
+
+function model(sequelize) {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: 'Must be a valid email address',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6],
+          msg: 'Minimum 6 characters required in last name',
+        },
+      },
+    },
+  };
+
+  const options = {
+    indexes: [
+      {
+        fields: ['email'],
+        unique: true,
+      },
+    ],
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSaltSync(10, 'a');
+          user.password = bcrypt.hashSync(user.password, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSaltSync(10, 'a');
+          user.password = bcrypt.hashSync(user.password, salt);
+        }
+      },
+    },
+    instanceMethods: {
+      validPassword: (password) => {
+        return bcrypt.compareSync(password, this.password);
+      },
+    },
+  };
+  const userSchema = sequelize.define('User', attributes, options);
+
+  userSchema.prototype.validPassword = async (password, hash) => {
+    return await bcrypt.compareSync(password, hash);
+  };
+
+  return userSchema;
+}
+
+module.exports = model;
