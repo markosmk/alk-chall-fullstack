52658aaf1cc3a15feacb886e26731361b5c51f46
Markos <markosmk@me.com>
2022-02-03 22:33:35 -0300

    update: handle all routes one place

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Panel } from './Panel/Panel';
+export { default as User } from './User/User';
+export { default as Categories } from './Categories/Categories';
+export { default as Login } from './Login/Login';
+export { default as NotFound } from './NotFound/NotFound';
