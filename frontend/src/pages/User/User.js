b2331b6874f1ba8a8283fcec1186faeafc954868
Markos <markosmk@me.com>
2022-02-04 22:59:42 -0300

    update: added isLoading for await data request

--- frontend/src/pages/User/User.js
+++ frontend/src/pages/User/User.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getInfoUser } from '../../context/reducers/userSlice';
 
 const User = () => {
-  const { userData } = useSelector((state) => state.user);
+  const { userData, isLoading } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,7 +13,11 @@ const User = () => {
   return (
     <div>
       <h1>Usuario</h1>
-      <pre>{userData && JSON.stringify(userData, null, ' ')}</pre>
+      {isLoading ? (
+        <pre>Cargando Datos...</pre>
+      ) : (
+        <pre>{userData && JSON.stringify(userData, null, ' ')}</pre>
+      )}
     </div>
   );
 };
