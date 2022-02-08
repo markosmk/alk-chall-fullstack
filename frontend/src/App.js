8a83cc999ca3aa3c53da67a0af103f13c422208c
Markos <markosmk@me.com>
2022-02-07 23:45:08 -0300

    update: add toast to app and tailwind to index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,20 @@
 import { BrowserRouter } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import toast, { ToastBar, Toaster } from 'react-hot-toast';
 
 import Router from './routes';
-import Header from './components/Header';
-import { logout } from './context/reducers/authSlice';
-
-import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthStatus />
-      <Header />
       <Router />
+      <Toaster position="top-right">
+        {(t) => (
+          <div style={{ cursor: 'pointer' }} onClick={() => toast.dismiss(t.id)}>
+            <ToastBar onClick={() => alert(1)} toast={t} />
+          </div>
+        )}
+      </Toaster>
     </BrowserRouter>
-  );
-}
-
-function AuthStatus() {
-  const { isLoggedIn, user } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-
-  if (!isLoggedIn) {
-    return <p>No has iniciado sesion.</p>;
-  }
-
-  return (
-    <p>
-      Bienvenido <strong>{user?.email}!</strong>
-      <button
-        onClick={() => {
-          dispatch(logout());
-        }}
-      >
-        Salir
-      </button>
-    </p>
   );
 }
 
