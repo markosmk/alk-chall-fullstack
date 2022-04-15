2e618d261b57ce9695cd1863a401779d213ffc32
Markos <markosmk@me.com>
2022-04-14 22:47:18 -0300

    update: clean component

--- frontend/src/pages/Panel/Panel.js
+++ frontend/src/pages/Panel/Panel.js
@@ -1,35 +1,37 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import HeaderPage from 'components/HeaderPage';
 
-import { openModal } from '../../context/reducers/modalSlice';
-import Filters from '../../components/Filters';
-import ListOperations from '../../components/ListOperations';
+import ListOperations from 'components/ListOperations';
+import { getOperations } from 'context/reducers/operationSlice';
+import { useEffect } from 'react';
 
 const Panel = () => {
   const dispatch = useDispatch();
+  const { list, isLoading, error } = useSelector((state) => state.operation);
+
+  useEffect(() => {
+    dispatch(getOperations());
+  }, [dispatch]);
 
   return (
-    <div>
-      <div className="mx-auto max-w-7xl rounded-t-lg bg-white py-16 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">Balance</h1>
-        <p className="mt-4 max-w-xl text-sm text-gray-700">
-          Muestra la tabla de los ingresos y egresos de/los usuarios
+    <>
+      <HeaderPage
+        title="Balance"
+        description="Se muestra una lista con las ultimas 10 operaciones ingresadas"
+        linkUrl="/operations/new"
+        linkText="Crear Operacion"
+      >
+        <p className="my-4">
+          <span className="text-lg text-gray-400">Balance Actual: </span>
+          <span>-/+</span>
+          <strong className="text-2xl">$12300.00</strong>
         </p>
-        <p>
-          Balance: <strong>$12300.00</strong>
-        </p>
-        <button
-          className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-          onClick={() => dispatch(openModal())}
-        >
-          Crear Nueva Operacion
-        </button>
-      </div>
-      {/* <Filters /> */}
+      </HeaderPage>
+
       <p className="py-4 px-4 text-gray-400 sm:px-6">Ultimas 10 operaciones</p>
 
-      <ListOperations />
-    </div>
+      <ListOperations isLoading={isLoading} operations={list} error={error} />
+    </>
   );
 };
 
