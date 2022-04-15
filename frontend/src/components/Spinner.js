6883fbeb788ed812d877672436e79bf47629e370
Markos <markosmk@me.com>
2022-04-14 22:48:10 -0300

    update: minor changes in css with tailwind

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -3,7 +3,7 @@ const Spinner = ({ centered }) => {
   const spinnerSvg = (
     <svg
       role="status"
-      className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-black"
+      className="mr-2 h-8 w-8 animate-spin fill-black text-gray-200 dark:text-gray-600"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +21,7 @@ const Spinner = ({ centered }) => {
   return (
     <>
       {centered ? (
-        <div className="flex items-center justify-center">{spinnerSvg}</div>
+        <div className="my-8 flex items-center justify-center">{spinnerSvg}</div>
       ) : (
         spinnerSvg
       )}
