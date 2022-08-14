73cccf1d850950ba4aa1bc76f4c1e2f583892a05
Markos <markosmk@me.com>
2022-08-13 23:55:03 -0300

    remove: old files with sequelize

--- backend/components/auth/auth.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { loginUser, registerUser } = require('./auth.controller');
-
-// routes
-router.post('/login', loginUser);
-router.post('/register', registerUser);
-
-module.exports = router;
